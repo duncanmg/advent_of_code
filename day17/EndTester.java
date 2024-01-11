c4e9b14b397b334c18abfdfb0e13de2f4deabb81
Duncan Garland <duncan.garlnd@ntlworld.com>
2024-01-11 09:23:58 +0000

    Log the label of the falling rock. Only call getCurrentHeight() once. Don't die on height error.

--- day17/EndTester.java
+++ day17/EndTester.java
@@ -31,14 +31,29 @@ class EndTester {
 	public JetGenerator jetGenerator;
 
 	public Boolean ended() throws Exception {
-		this.logger.log("Testing end conditions. maxRocks=" + this.maxRocks + ", maxHeight=" + this.maxHeight + ".");
-		this.logger.log("Rocks: " + this.chamber.rocks.size() + ", Height: " + this.chamber.getCurrentHeight() + ".");
+		int numRocks = this.chamber.rocks.size();
+
+		char fallingRockLabel = ' ';
+		if (numRocks > 0) {
+			fallingRockLabel = this.chamber.getFallingRock().label;
+		}
+
+		int currentHeight = this.chamber.getCurrentHeight();
+		this.logger.log("Testing end conditions. fallingRock=" + fallingRockLabel + " maxRocks=" + this.maxRocks + ", maxHeight=" + this.maxHeight + ".");
+		this.logger.log("Rocks: " + numRocks + ", Height: " + currentHeight + ".");
 		this.logger.log("Jets: " + this.jetGenerator.totalJetsGenerated + ", maxJets: " + this.maxJets + ".");
-		if (this.maxRocks != null && this.chamber.rocks.size() >= this.maxRocks) {
+
+		if (this.maxRocks != null && numRocks >= this.maxRocks) {
+			if (numRocks != this.maxRocks) {
+				// throw new Exception("maxRocks exceeded. maxRocks=" + this.maxRocks + ", rocks=" + numRocks + ".");
+			}
 			this.logger.log("End. maxRocks reached.");
 			return true;
 		}
-		if (this.maxHeight != null && this.chamber.getCurrentHeight() >= this.maxHeight) {
+		if (this.maxHeight != null && currentHeight >= this.maxHeight) {
+			if (currentHeight != this.maxHeight) {
+				// throw new Exception("maxHeight exceeded. maxHeight=" + this.maxHeight + ", currentHeight=" + currentHeight + ".");
+			}
 			this.logger.log("End. maxHeight reached.");
 			return true;
 		}
