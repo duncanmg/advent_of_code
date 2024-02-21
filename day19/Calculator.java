4b58a1f624aa03ebf92efc18a0b188981008c154
Duncan Garland <duncan.garlnd@ntlworld.com>
2024-02-21 17:27:50 +0000

    Added some documentation and refactored it to work with the new robot structure.

--- day19/Calculator.java
+++ day19/Calculator.java
@@ -13,14 +13,37 @@ class Calculator {
 	}
 
 	public Calculator(RobotStrategy rs) {
-		robotStrategy = rs;
-		blueprint = rs.blueprint;
+		robotStrategy = (RobotStrategy) rs.clone();
+
+		blueprint = robotStrategy.blueprint;
+
+		HashMap<String, Robot> robots = robotStrategy.robots;
+		oreRobot = (OreRobot) robots.get("ore");
+		clayRobot = (ClayRobot) robots.get("clay");
+		obsidianRobot = (ObsidianRobot) robots.get("obsidian");
+		geodeRobot = (GeodeRobot) robots.get("geode");
 	}
 
 	Logger logger = new Logger(this, true);
 
 	RobotStrategy robotStrategy;
 
+	OreRobot oreRobot;
+
+	ClayRobot clayRobot;
+
+	ObsidianRobot obsidianRobot;
+
+	GeodeRobot geodeRobot;
+
+	// This attempts to determine whether there is time to create another geode
+	// robot and returns false if there isn't enough time.
+	//
+	// The estimate of time required will be more accurate as time progresses, so
+	// this will return true unless the current minute is minSearchTime or greater.
+	//
+	// It adds a safety margin to the amount of time remaining.
+	//
 	boolean continueSearching() {
 		float minSearchTime = (float) robotStrategy.maxMinutes/2;
 		float safetyMarginMinutes = (float) robotStrategy.maxMinutes/4;
@@ -60,7 +83,7 @@ class Calculator {
 	}
 
 	float timeToOreRobot() {
-		float timeToOreRobot = timeToCreateRobot + ((blueprint.oreRobotCost - robotStrategy.oreTotal) / robotStrategy.numOreRobots);
+		float timeToOreRobot = timeToCreateRobot + ((blueprint.oreRobotCost - oreRobot.total) / oreRobot.numRobots);
 		if (timeToOreRobot < 0) {
 			timeToOreRobot = 0;
 		}
@@ -68,7 +91,7 @@ class Calculator {
 	}
 
 	float timeToClayRobot() {
-		float timeToClayRobot = timeToCreateRobot + ((blueprint.clayRobotCost - robotStrategy.oreTotal) / robotStrategy.numOreRobots);
+		float timeToClayRobot = timeToCreateRobot + ((blueprint.clayRobotCost - oreRobot.total) / oreRobot.numRobots);
 		if (timeToClayRobot < 0) {
 			timeToClayRobot = 0;
 		}
@@ -77,15 +100,15 @@ class Calculator {
 
 	float timeToObsidianRobot(float timeToClayRobot) {
 		float timeToObsidianRobot = (float) 0.0;
-		float oreTime = (blueprint.obsidianRobotOreCost - robotStrategy.oreTotal) / robotStrategy.numOreRobots;
-		logger.log("oreTime " + oreTime + " blueprint.obsidianRobotOreCost " + blueprint.obsidianRobotOreCost + " oreTotal " + robotStrategy.oreTotal);
-		if (robotStrategy.numClayRobots == 0) {
+		float oreTime = (blueprint.obsidianRobotOreCost - oreRobot.total) / oreRobot.numRobots;
+		logger.log("oreTime " + oreTime + " blueprint.obsidianRobotOreCost " + blueprint.obsidianRobotOreCost + " oreTotal " + oreRobot.total);
+		if (clayRobot.numRobots == 0) {
 			float clayTime = timeToClayRobot + blueprint.obsidianRobotClayCost + timeToCreateRobot;
 			timeToObsidianRobot = Math.max(oreTime, clayTime);
 			logger.log("timeToObsidianRobot " + timeToObsidianRobot + " oreTime " + oreTime + " clayTime " + clayTime);
 		}
 		else {
-			float clayTime = timeToCreateRobot + ((blueprint.obsidianRobotClayCost - robotStrategy.clayTotal) / robotStrategy.numClayRobots);
+			float clayTime = timeToCreateRobot + ((blueprint.obsidianRobotClayCost - clayRobot.total) / clayRobot.numRobots);
 			timeToObsidianRobot = Math.max(oreTime, clayTime);
 			logger.log("timeToObsidianRobot " + timeToObsidianRobot + " oreTime " + oreTime + " clayTime " + clayTime);
 		}
@@ -99,13 +122,13 @@ class Calculator {
 
 	float timeToGeodeRobot(float timeToObsidianRobot) {
 		float timeToGeodeRobot = (float) 0.0;
-		float oreTime = (blueprint.geodeRobotOreCost - robotStrategy.oreTotal) / robotStrategy.numOreRobots;
-		if (robotStrategy.numObsidianRobots == 0) {
+		float oreTime = (blueprint.geodeRobotOreCost - oreRobot.total) / oreRobot.numRobots;
+		if (obsidianRobot.numRobots == 0) {
 			float obsidianTime = timeToObsidianRobot + blueprint.geodeRobotObsidianCost + timeToCreateRobot;
 			timeToGeodeRobot = Math.max(oreTime, obsidianTime);
 		}
 		else {
-			float obsidianTime = timeToCreateRobot + ((blueprint.geodeRobotObsidianCost - robotStrategy.obsidianTotal) / robotStrategy.numObsidianRobots);
+			float obsidianTime = timeToCreateRobot + ((blueprint.geodeRobotObsidianCost - obsidianRobot.total) / obsidianRobot.numRobots);
 			timeToGeodeRobot =  Math.max(oreTime, obsidianTime);
 		}
 
